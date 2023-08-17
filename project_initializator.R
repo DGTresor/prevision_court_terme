d17bcc0a368ececf6a5882c5037a7b30ce1995c3
Louise Phung <louise.phung@dgtresor.gouv.fr>
2023-08-17 10:52:31 +0200

    put project_initializator.R at the project root rather than in the ./code folder + small refacto

--- project_initializator.R
+++ project_initializator.R
@@ -3,23 +3,19 @@
 # Created by: lphung
 # Created on: 17/08/2023
 
-# please specify if you are a user within or without the French Treasury
-IN_FRENCH_TREASURY <- TRUE
-
 # this project has been developed with R.4.0.2.
 message("Ce projet a été développé avec la version R 4.0.2.")
 
 # create the project architecture
 dir.create("./output")
-if (IN_FRENCH_TREASURY) {
-  # Note: this folder will not be usefull for individuals external to the French Treasury
-  dir.create("./data")
-}
+# Note: this folder will not be usefull for individuals external to the French Treasury
+dir.create("./data")
+
 
 # package management
 ## package manager: https://cran.r-project.org/web/packages/renv/vignettes/renv.html; https://rstudio.github.io/renv/articles/renv.html
 ## load package manager, if needed
-if(!("renv" %in% rownames(installed.packages()))){
+if (!("renv" %in% rownames(installed.packages()))) {
   install.packages("renv")
 }
 
